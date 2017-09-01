a296d9ca3d19e78be03c8077c1b6e80ea5ce8d39
mike <msheehan79@yahoo.com>
2017-08-31 23:34:25 -0400

    Add a simple shutdown script that is tied to a contact switch on GPIO pins 3 + 5

---
+++ home/pi/scripts/halt-and-restart.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# shutdown/startup script
+
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+DEBUG = False
+
+BUTTON = 5
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(BUTTON, GPIO.IN)
+
+while True:
+        GPIO.wait_for_edge(BUTTON, GPIO.FALLING)
+
+        if DEBUG:
+                print("Button press detected")
+        else:
+                print("Shutdown process initiated")
+                subprocess.call("/sbin/shutdown -h now", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
