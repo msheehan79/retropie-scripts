748ba578b7d5c1c8dc5eebc5c788c78126c255fe
mike <msheehan79@yahoo.com>
2017-12-20 00:03:10 -0500

    Only kill xboxdrv if we are going to be running it again

--- home/pi/scripts/xboxdrv.sh
+++ home/pi/scripts/xboxdrv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo killall -9 > /dev/null 2>&1 xboxdrv
 joycommand="$(sed '5!d; s/..$//' /dev/shm/runcommand.info)"
 if [[ -n "$joycommand" ]]; then
+	sudo killall -9 > /dev/null 2>&1 xboxdrv
 	eval $joycommand
 fi
