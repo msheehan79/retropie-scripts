f561a5d2a5fbd5ecc26a5a6e881daee26409af4e
mike <msheehan79@yahoo.com>
2018-03-11 17:42:39 -0400

    Add libretro configs for lr-mupen64plus and lr-ppsspp, and don't use xboxdrv for standalone versions of both cores.

--- opt/retropie/configs/all/runcommand-onstart.sh
+++ opt/retropie/configs/all/runcommand-onstart.sh
@@ -135,9 +135,32 @@ case $1 in
 	;;
 
 	n64)
-		$xboxkill
+		case $2 in
+			"lr-mupen64plus")
+				$xboxkill
+				joycommand="$basicConfig ${player1['id']} ${player1['map']} &"
+				if [ ${#player2[@]} -gt 0 ]; then
+					joycommand="$joycommand $basicConfig ${player2['id']} ${player2['map']} &"
+				fi
+
+				echo $joycommand >> /dev/shm/runcommand.info
+				eval $joycommand
+				# Need to put a delay to allow xboxdrv to get set up before we can poll for the SDL IDs to map those
+				sleep 0.5
+				map_joystick_indexes		
+				sed -i "s/input_player1_joypad_index\s*\=\s*.*/input_player1_joypad_index = ${player1['jsindex']}/g" /opt/retropie/configs/all/retroarch.cfg
+				sed -i "s/input_player1_joypad_index\s*\=\s*.*/input_player1_joypad_index = ${player1['jsindex']}/g" /opt/retropie/configs/$1/retroarch.cfg
+				if [ ${#player2[@]} -gt 0 ]; then
+					sed -i "s/input_player2_joypad_index\s*\=\s*.*/input_player2_joypad_index = ${player2['jsindex']}/g" /opt/retropie/configs/all/retroarch.cfg
+					sed -i "s/input_player2_joypad_index\s*\=\s*.*/input_player2_joypad_index = ${player2['jsindex']}/g" /opt/retropie/configs/$1/retroarch.cfg
+				fi
+			;;
+			*)
+				$xboxkill
+			;;
+		esac
 	;;
-	
+
 	nds)
 		$xboxkill
 		joycommand="$basicConfig ${player1['id']} ${player1['map']} $nds &"
@@ -166,6 +189,26 @@ case $1 in
 		fi
 	;;
 
+	psp)
+        case $2 in
+			"lr-ppsspp")
+				$xboxkill
+				joycommand="$basicConfig ${player1['id']} ${player1['map']} &"
+
+				echo $joycommand >> /dev/shm/runcommand.info
+				eval $joycommand
+				# Need to put a delay to allow xboxdrv to get set up before we can poll for the SDL IDs to map those
+				sleep 0.5
+				map_joystick_indexes		
+				sed -i "s/input_player1_joypad_index\s*\=\s*.*/input_player1_joypad_index = ${player1['jsindex']}/g" /opt/retropie/configs/all/retroarch.cfg
+				sed -i "s/input_player1_joypad_index\s*\=\s*.*/input_player1_joypad_index = ${player1['jsindex']}/g" /opt/retropie/configs/$1/retroarch.cfg	
+			;;
+			"ppsspp")
+				$xboxkill
+			;;
+		esac
+	;;
+	
 	arcade)
         case $2 in
 			"lr-mame2003"|"lr-mame2003-plus"|"lr-mame2010"|"lr-fbalpha")
@@ -306,7 +349,7 @@ case $1 in
 				fi
 				echo $joycommand >> /dev/shm/runcommand.info
 				eval $joycommand
-			;;
+			;;		
 			*)
 				$xboxkill
 				joycommand="$basicConfig ${player1['id']} ${player1['map']} $advmame_p1 &"
